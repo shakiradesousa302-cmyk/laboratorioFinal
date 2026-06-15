5ca32cc49c64619a10fc8164c0c8a16f66ed5d62
gabs <shakiradesousa302@gmail.com>
2026-06-15 16:11:47 -0600

    hi

--- Producto.java
+++ Producto.java
@@ -2,4 +2,46 @@ package proyecto;
 
 public class Producto {
 
+	 private String nombre;
+	    private double precioCompra;
+	    private double precioVenta;
+	    private int cantidadMax;
+	    private int cantidadMin;
+	    private Categoria categoria;
+
+	    public Producto(int id, String nombre, double pC, double pV, Categoria cat) {
+	        this.idProducto = id;
+	        this.nombre = nombre;
+	        this.precioCompra = pC;
+	        this.precioVenta = pV;
+	        this.categoria = cat;
+	    }
+
+	    public int getIdProducto() { return idProducto; }
+	    public void setIdProducto(int id) { this.idProducto = id; }
+
+	    public String getNombre() { return nombre; }
+	    public void setNombre(String nombre) { this.nombre = nombre; }
+
+	    public double getPrecioCompra() { return precioCompra; }
+	    public void setPrecioCompra(double precio) { this.precioCompra = precio; }
+
+	    public double getPrecioVenta() { return precioVenta; }
+	    public void setPrecioVenta(double precio) { this.precioVenta = precio; }
+
+	    public int getCantidadMax() { return cantidadMax; }
+	    public void setCantidadMax(int cant) { this.cantidadMax = cant; }
+
+	    public int getCantidadMin() { return cantidadMin; }
+	    public void setCantidadMin(int cant) { this.cantidadMin = cant; }
+
+	    public Categoria getCategoria() { return categoria; }
+	    public void setCategoria(Categoria cat) { this.categoria = cat; }
+
+	    @Override
+	    public String toString() {
+	        return "Producto [ID=" + idProducto + ", Nombre=" + nombre + ", Precio Venta=$" + precioVenta + "]";
+	    }
+	
+
 }
