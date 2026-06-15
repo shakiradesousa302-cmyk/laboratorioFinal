c5c4d2181cb090db7ddbc7566ba5b1022531d9d9
Riot2010 <armandoenmanuel12@gmail.com>
2026-06-15 15:14:27 -0600

    Clase cliente y clase empleado terminados

--- Cliente.java
+++ Cliente.java
@@ -1,5 +1,33 @@
 package proyecto;
 
 public class Cliente {
+	    private int idCliente;
+	    private String nombre;
+	    private String direccion;
+	    private String telefono;
 
+	    public Cliente(int id, String nombre, String tel) {
+	        this.idCliente = id;
+	        this.nombre = nombre;
+	        this.telefono = tel;
+	        this.direccion = "";
+	    }
+
+	    public int getIdCliente() { return idCliente; }
+	    public void setIdCliente(int id) { this.idCliente = id; }
+
+	    public String getNombre() { return nombre; }
+	    public void setNombre(String nombre) { this.nombre = nombre; }
+
+	    public String getDireccion() { return direccion; }
+	    public void setDireccion(String dir) { this.direccion = dir; }
+
+	    public String getTelefono() { return telefono; }
+	    public void setTelefono(String tel) { this.telefono = tel; }
+
+	    @Override
+	    public String toString() {
+	        return "Cliente [ID=" + idCliente + ", Nombre=" + nombre + ", Teléfono=" + telefono + ", Dirección=" + direccion + "]";
+	    }
+	}
 }
--- Empleado.java
+++ Empleado.java
@@ -1,5 +1,44 @@
 package proyecto;
 
 public class Empleado {
+	  private int idEmpleado;
+	    private String nombre;
+	    private String cargo;
+	    private String direccion;
+	    private String fechaIngreso;
+	    private String horarioEntrada;
+	    private String horarioSalida;
 
+	    public Empleado(int id, String nombre, String cargo) {
+	        this.idEmpleado = id;
+	        this.nombre = nombre;
+	        this.cargo = cargo;
+	        this.direccion = "";
+	        this.fechaIngreso = "";
+	        this.horarioEntrada = "";
+	        this.horarioSalida = "";
+	    }
+
+	    public int getIdEmpleado() { return idEmpleado; }
+	    public void setIdEmpleado(int id) { this.idEmpleado = id; }
+
+	    public String getNombre() { return nombre; }
+	    public void setNombre(String nombre) { this.nombre = nombre; }
+
+	    public String getCargo() { return cargo; }
+	    public void setCargo(String cargo) { this.cargo = cargo; }
+
+	    public String getDireccion() { return direccion; }
+	    public void setDireccion(String dir) { this.direccion = dir; }
+
+	    public String getFechaIngreso() { return fechaIngreso; }
+
+	    public String getHorarioEntrada() { return horarioEntrada; }
+	    public String getHorarioSalida() { return horarioSalida; }
+
+	    @Override
+	    public String toString() {
+	        return "Empleado [ID=" + idEmpleado + ", Nombre=" + nombre + ", Cargo=" + cargo + "]";
+	    }
+	}
 }
